f03565ae621264a0dd41ab713248280a791e039f
Marcos Marinho <marcosv.rmarinho@gmail.com>
2021-01-29 20:17:10 -0300

    Calculate how much rise the user will recive
    Asks the user to inform his earnings, and  calculate a fifteen percent rise on it

---
+++ Guanabara/ex013.py
@@ -0,0 +1,5 @@
+print('Para calcular o aumento de 15% no salario', end='. ')
+a = float(input('Informe quanto recebe atualmente:\n R$'))
+b = (float(a * 1.15))
+print('Com aumento de 15% o novo salario será de R${:.2f}'.format(b))
+
