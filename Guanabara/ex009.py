5834ab84b6e91a16ac0a4008ac07504b3bfc34f4
Marcos Marinho <marcosv.rmarinho@gmail.com>
2021-01-28 22:17:55 -0300

    Does multiplication table

---
+++ Guanabara/ex009.py
@@ -0,0 +1,27 @@
+print('Qual número deseja verificar a tabuada?', end=' ')
+a = int(input(':'))
+a0 = a * 0
+a1 = a * 1
+a2 = a * 2
+a3 = a * 3
+a4 = a * 4
+a5 = a * 5
+a6 = a * 6
+a7 = a * 7
+a8 = a * 8
+a9 = a * 9
+a10 = a * 10
+print('A taboada de {} é:\n--------'.format(a))
+print(a, 'x 0 = {}'.format(a0))
+print(a, 'x 1 = {}'.format(a1))
+print(a, 'x 2 = {}'.format(a2))
+print(a, 'x 2 = {}'.format(a2))
+print(a, 'x 3 = {}'.format(a3))
+print(a, 'x 4 = {}'.format(a4))
+print(a, 'x 5 = {}'.format(a5))
+print(a, 'x 6 = {}'.format(a6))
+print(a, 'x 7 = {}'.format(a7))
+print(a, 'x 8 = {}'.format(a8))
+print(a, 'x 9 = {}'.format(a9))
+print(a, 'x 10={} \n---------'.format(a10))
+
