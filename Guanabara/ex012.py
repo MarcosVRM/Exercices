2380d602d4a5d852eaa7290a6415eeab24e8b12c
Marcos Marinho <marcosv.rmarinho@gmail.com>
2021-01-29 20:06:59 -0300

    Ask the value of a item and gives a discound
    Ask who much a item cust, and calculate a five percent discound

---
+++ Guanabara/ex012.py
@@ -0,0 +1,3 @@
+a = float(input('Qual o valor do produto deja saber o valor com desconto? \nR$'))
+b = float(a * 0.95)
+print('O produto que custa R${}, com o desconto de 5% custara R${:.2f}'.format(a, b))
