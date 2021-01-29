230d55bead3cd11f1f64c478378b0db577601fdd
Marcos Marinho <marcosv.rmarinho@gmail.com>
2021-01-28 22:06:20 -0300

    Calculate double, triple and square root of number given

---
+++ Guanabara/ex006.py
@@ -0,0 +1,6 @@
+print('Agora vamos calcular o dobro, triplo e raiz quadrada!')
+a= int(input('Digite um número:'))
+ad = a * 2
+at = a * 3
+ar = a **(1/2)
+print('O valor informado foi {}, seu dobro é {}, o triplo {} e a raiz quadrada {}!'.format(a, ad, at, ar))
