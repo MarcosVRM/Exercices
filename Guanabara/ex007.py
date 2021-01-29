a6b305fc899c5cecbd7b98944bd867926318b64d
Marcos Marinho <marcosv.rmarinho@gmail.com>
2021-01-28 22:10:30 -0300

    Calculate  average

---
+++ Guanabara/ex007.py
@@ -0,0 +1,5 @@
+print('Vamos verificar a media de valores, no caso duas notas escolares')
+a = int(input('Digite o valor da primeira nota:'))
+b = int(input('Digite o valor da segunda nota:'))
+abm = (a + b)/2
+print('A media das notas {}, {} é de {}.'.format(a, b , abm))
