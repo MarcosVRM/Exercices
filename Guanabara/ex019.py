19a2d09a32018427cd4688029fb01278eba0e879
Marcos Marinho <marcosv.rmarinho@gmail.com>
2021-01-29 20:42:05 -0300

    select a name from a list

---
+++ Guanabara/ex019.py
@@ -0,0 +1,8 @@
+import random
+n1 = str('marcos')
+n2 = str('vincios')
+n3 = str('rosario')
+n4 = str('marinho')
+lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
+print('o aluno escolhido foi {}'.format(escolhido))
