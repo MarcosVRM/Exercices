fb31c670895d31665c16ede2117e8693d7bc01a9
Marcos Marinho <marcosv.rmarinho@gmail.com>
2021-01-28 22:04:28 -0300

    Check whos the next and last number

---
+++ Guanabara/ex005.py
@@ -0,0 +1,5 @@
+print('Vamos verificar qual o antecessor e o sucessor de um número!')
+a = int(input('Digite um Número:'))
+a1 = a - 1
+a2 = a + 1
+print('O valor digitado é {}, seu antecessor é {} e  o sucessor é {}!'.format(a, a1, a2))
