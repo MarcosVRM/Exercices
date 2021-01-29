d9da8ffaa05db3aa196c2a30598836ebf5229177
Marcos Marinho <marcosv.rmarinho@gmail.com>
2021-01-28 22:12:54 -0300

    converts meters to centimeters and millimeters

---
+++ Guanabara/ex008.py
@@ -0,0 +1,5 @@
+print('Vamos converter metros em centimetros e em milimetros.')
+a = int(input('Quanntos metros deseja converter? \n Metros:'))
+ac = a * 100
+am = a * 1000
+print('{}m, equivale a {}cm e a {}mm.'.format(a, ac, am))
